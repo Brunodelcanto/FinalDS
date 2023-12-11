6604d7587a6db1aed89fff8251aff922a67225bb
Bruno <delcantobruno5@gmail.com>
2023-12-11 15:42:39 -0300

    carrousel y cards

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -6,51 +6,66 @@ import 'bootstrap/dist/css/bootstrap.min.css';
         return(
             <body>
     <div>         
-      <ul class="listaMenu">
-  <li class="items">
-    <a class="link " href="#">Inicio</a>
+      <ul className="listaMenu">
+  <li className="items">
+    <a className="link " href="#">Inicio</a>
   </li>
-  <li class="items">
-    <a class="link" href="#">Productos</a>
+  <li className="items">
+    <a className="link" href="#">Productos</a>
   </li>
-  <li class="items">
-    <a class="link" href="#" >Tips</a>
+  <li className="items">
+    <a className="link" href="#" >Tips</a>
   </li>
-  <li class="items">
-    <a class="link" href="#">Contacto</a>
+  <li className="items">
+    <a className="link" href="#">Contacto</a>
   </li>
-   <li class="items">
-    <a class="link" href="#">Sobre Nosotros</a>
+   <li className="items">
+    <a className="link" href="#">Sobre Nosotros</a>
   </li>
 </ul>
 </div> 
 
-<div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/portamate.jpg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="/mate.jpg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="/yerbera.jpg" class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+<div className="slider-frame">
+   <ul>
+    <li><img src="/portamate.jpg" alt="" /></li>
+    <li><img src="/mate.jpg" alt="" /></li>
+    <li><img src="/yerbera.jpg" alt="" /></li>
+   </ul>
 </div>
 
 <div className="Bienvenidos">
     <h2>Bienvenidos a Che el Mate</h2>  
 </div>
 
+<div className="productos">
+<div className="card">
+<img src="/portamate.jpg" class="card-img-top" alt="..."/>
+<div className="card-body">
+    <h5 className="card-title">Kit matero</h5>
+    <p clasNames="card-text"> Kit portamate, termo y bombilla, hecho en cuero negro</p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+  </div>
+  
+  <div className="card">
+<img src="/mate.jpg" class="card-img-top" alt="..."/>
+<div className="card-body">
+    <h5 className="card-title">Mate camionero de algarrobo</h5>
+    <p clasNames="card-text">Lorem ipsum dolor sit</p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+  </div>
+
+  <div className="card">
+<img src="/yerbera.jpg" class="card-img-top" alt="..."/>
+<div className="card-body">
+    <h5 className="card-title">Combo yerbera y azucarera</h5>
+    <p clasNames="card-text">Lorem ipsum dolor sit</p>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+  </div>
+  </div>
+  
 <div className="Instagram">
 <span className="nombreIg"><img src="./instagram2.png" alt="" className="ig" />che_el_mate</span>
 <span className="estamos">Estamos en instagram</span>
